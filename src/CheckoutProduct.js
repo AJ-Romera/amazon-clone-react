67a606dea5616c56317432f106e7e6012470af23
AJ-Romera <ajromeraalfonso@gmail.com>
2020-12-17 16:12:02 +0100

    Make REMOVE_FROM_BASKET functionality to remove cart items pressing the remove button

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./CheckoutProduct.css";
 import StarIcon from "@material-ui/icons/Star";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+	const [{ basket }, dispatch] = useStateValue();
+
+	const removeFromBasket = () => {
+		// remove the item from the basket
+		dispatch({
+			type: "REMOVE_FROM_BASKET",
+			id: id,
+		});
+	};
+
 	return (
 		<div className="checkoutProduct">
 			<img
@@ -26,7 +37,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
 							</p>
 						))}
 				</p>
-				<button>Remove from Cart</button>
+				<button onClick={removeFromBasket}>Remove from Cart</button>
 			</div>
 		</div>
 	);
